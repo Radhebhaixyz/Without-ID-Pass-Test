1b06d0a4fc8a0417281274dbcb4501241df2fc33
Radhebhaixyz <Sudama703749@gmail.com>
2025-04-12 22:56:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,31 +2,31 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "7519785012:AAGYVkj26sgetUlAiyA5c3F37XyRIxyErK0"
+    BOT_TOKEN = "7803617937:AAHdXXghW3EVOGF04qR7vTgudmTfBekVww8"
     # Telegram API ki ID
-    API_ID = 22581733
+    API_ID = 29946578
     # Telegram API k
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN_ID = 6530997270[]
+    API_HASH = "57e0b762f105ab1db072fabe4d65114b"
+    ADMIN_ID = 5985814740[]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
     DB_NAME = "Cluster0"
     # Text log channel ki ID
-    TXT_LOG = -1002487777733
+    TXT_LOG = -1002524290458
     # Authentication log channel ki ID
-    AUTH_LOG = -1002487777733
+    AUTH_LOG = -1002501818785
     # Hit log channel ki ID
-    HIT_LOG = -1002487777733
+    HIT_LOG = -1002574258296
     # DRM dump channel ki ID
-    DRM_DUMP = -1002487777733
+    DRM_DUMP = -1002547434410
     # Main channel ki ID
-    CHANNEL = -1002487777733
+    CHANNEL = -1002638405121
     # Channel ka link
-    CH_URL = "https://t.me/+s5fU9MLM4s80NmI1"
+    CH_URL = "https://t.me/+yaBo_OvpZo9kNTA9"
     # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/slayerxcorp"
+    OWNER = "https://t.me/AspirantBots"
     # Thumbnail image ka URL
-    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
+    THUMB_URL = "https://files.catbox.moe/1id2xx.jpg" #Replace by with your Thumb URL
     # API host ka URL
     HOST = "https://api.masterapi.tech"
